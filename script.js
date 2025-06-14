f67579b46735c7c9cd661291f8280da0861c9435
braudesDev <braudesdev@gmail.com>
2025-06-13 23:52:53 -0600

    S estan actualizando fuentes en titulos y solo detalles graficos

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ window.setVolumen = function(valor) {
 // ======================================
 
 // Fecha de la fiesta
-const fechaFiesta = new Date("2025-03-06T00:44:00").getTime();
+const fechaFiesta = new Date("2025-07-12T15:00:00").getTime();
 
 // Obtener el contenedor de la sección "Subir Fotos"
 const sectionSubirFotos = document.getElementById("subir-fotos"); // Asegúrate de que la sección tiene el ID "subir-fotos"
@@ -170,9 +170,6 @@ const intervalo = setInterval(function() {
     }
 
 }, 1000);
-
-
-
 
 
 // ======================================
